fc93d6767de648b5c23265c099fd5431c17c324a
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-12-08 17:00:49 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AppRouter } from "./app/router/AppRouter";
 import { Plogin } from "./app/views/Plogin";
 
 function App() {
-  useEffect(() => {
+  /*  useEffect(() => {
     // Redirección de HTTP a HTTPS
     if (
       window.location.protocol !== "https:" &&
@@ -14,11 +14,7 @@ function App() {
         "https://" + window.location.hostname + window.location.pathname;
     }
 
-    const handleBeforeUnload = (event: any) => {
-      const message = "¿Estás seguro de que deseas salir?";
-      event.returnValue = message; // Estándar para la mayoría de los navegadores
-      return message; // Necesario para algunos navegadores más antiguos
-    };
+   
 
     if (window.location.hostname !== "localhost") {
       const bloquearClickDerecho = (e: MouseEvent) => {
@@ -26,14 +22,12 @@ function App() {
       };
       // Agregar el escucha de evento para el click derecho en el montaje del componente
       window.addEventListener("contextmenu", bloquearClickDerecho);
-      window.addEventListener("beforeunload", handleBeforeUnload);
       // Remover el escucha de evento cuando el componente se desmonta
       return () => {
         window.removeEventListener("contextmenu", bloquearClickDerecho);
-        window.removeEventListener("beforeunload", handleBeforeUnload);
       };
     }
-  }, []);
+  }, []);*/
 
   return (
     <div>
